fc530738a5fd7ecb9878fe5b740aac686a610df2
ROBERTSPHXINTERNET <robert.s@phxinternet.com>
2024-10-28 20:31:03 -0700

    Added an about page

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return "Hello, Zander!"
 
+@app.route('/about')
+def about():
+    return "About this app"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
